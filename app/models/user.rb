7416dfaec8205db0c727b1fe1f34c4ee0d12d2c3
Creighton Higgins <chiggins@chiggins.com>
2014-10-06 16:56:00 -0700

    attribute to test is email, email validates uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ActiveRecord::Base
+
   include ActiveModel::ForbiddenAttributesProtection
 
   validates :first_name, :presence => true
   validates :last_name,  :presence => true
-  validates :email,      :presence => true
+  validates :email,      :presence => true,
+                         :uniqueness => true
+
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,11 @@ describe User do
 
   it { should validate_presence_of :first_name }
   it { should validate_presence_of :last_name }
-  it { should validate_presence_of :email_address }
-  it { should validate_uniqueness_of :email_address }
+  it { should validate_presence_of :email }
+  it { should validate_uniqueness_of :email }
 end
+
+# NOTE: both db/migrate and the model object user 'email', but if the
+#       test was in fact correct and the implementation was wrong,
+#       revert this change and update model/user and
+#       db/migrate/(date)_create_user (or alter user)
